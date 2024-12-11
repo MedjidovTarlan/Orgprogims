169c9b1eb038948d98d6d1cde3e40ae84871ca44
MedjidovTarlan <medjidovtarlan29@gmail.com>
2024-12-11 21:55:26 +0300

    Add files via upload

---
+++ module_1_4(orgprogims).py
@@ -0,0 +1,10 @@
+my_string=("Сколько тебе лет?")
+print(len(my_string))
+print("Сколько тебе лет?".upper())
+print("Сколько тебе лет?".lower())
+print("Сколько тебе лет?".replace(' ', ''))
+print(my_string[-1])
+
+
+
+
